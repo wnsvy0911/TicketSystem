ab9ab5ebcbf1fc917c633592e281e642e531a242
Junpyo Kim <jpkimkorea@gmail.com>
2022-02-28 01:57:44 -0600

    Create TicketFile class

---
+++ TicketFile.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace TicketSystem
+{
+    public class TicketFile
+
+    {
+        // public property
+        public string filePath { get; set; }
+
+        // constructor is a special method that is invoked
+        // when an instance of a class is created
+        public TicketFile(string ticketFilePath)
+        {
+            filePath = ticketFilePath;
+        }
+    }
+}
