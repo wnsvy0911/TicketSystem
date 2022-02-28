dca57ef3835e162c2e2c90ef562d396dec420004
Junpyo Kim <jpkimkorea@gmail.com>
2022-02-27 22:36:17 -0600

    Make sure Tickets.csv file exists

--- Program.cs
+++ Program.cs
@@ -16,6 +16,17 @@ namespace TicketSystem
             logger.Info("Program started");
 
             Console.WriteLine("Hello World!");
+            string file = "Tickets.csv";
+             // make sure movie file exists
+            if (!File.Exists(file))
+            {
+                logger.Error("File does not exist: {File}", file);
+            }
+            else
+            {
+                // TODO: create user menu
+            }
+
 
             logger.Info("Program ended");
         }
